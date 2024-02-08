ad5da27709adb28fdd4db43123683be36083d0e3
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2024-02-08 14:54:11 +0100

    remove build folder

--- build/firefox/extension-pop-up/self-test-overlay-restore.js
+++
@@ -1,5 +0,0 @@
-var overlay = document.getElementById("sovendusOverlay");
-overlay.style.display = "block";
-overlay.classList.remove("fullscreen");
-var overlayToggle = document.getElementById("toggleSovendusOverlay");
-overlayToggle.style.display = "block";
--- build/firefox/service-worker.js
+++
@@ -1,7 +0,0 @@
-browser.tabs.onUpdated.addListener(function (tabId, changeInfo) {
-  if (changeInfo.status == "complete") {
-    browser.tabs.executeScript(tabId, {
-      file: "page-banner/self-test-overlay-script-loader.js",
-    });
-  }
-});
