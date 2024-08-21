ad0d249a704c471bb3117107fe237af4a7656085
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2024-08-21 17:48:43 +0200

    improve tests

--- src/tests/page-banner/self-tester/firstName.test.ts
+++ src/tests/page-banner/self-tester/firstName.test.ts
@@ -3,11 +3,12 @@ import {
   StatusMessageKeyTypes,
 } from "@src/page-banner/self-tester";
 import { executeOverlayTests } from "../../testUtils";
+import { sovAppDataEverythingIsOkay, sovAppDataNoParameterButIsOkay } from "../sovAppData";
 
-executeOverlayTests(
-  "firstNameSuccess",
-  "everythingIsOkay.html",
-  async (driver, sovSelfTester) => {
+executeOverlayTests({
+  testName: "firstNameSuccess",
+  sovAppData: sovAppDataEverythingIsOkay,
+  testFunction: async (driver, sovSelfTester) => {
     expect(sovSelfTester.consumerFirstName.elementValue).toBe("John");
     expect(sovSelfTester.consumerFirstName.statusCode).toBe(
       StatusCodes.Warning
@@ -15,17 +16,17 @@ executeOverlayTests(
     expect(sovSelfTester.consumerFirstName.statusMessageKey).toBe(
       StatusMessageKeyTypes.consumerFirstNameSuccess
     );
-  }
-);
+  },
+});
 
-executeOverlayTests(
-  "firstNameMissing",
-  "noParameterButOkay.html",
-  async (driver, sovSelfTester) => {
+executeOverlayTests({
+  testName: "firstNameMissing",
+  sovAppData: sovAppDataNoParameterButIsOkay,
+  testFunction: async (driver, sovSelfTester) => {
     expect(sovSelfTester.consumerFirstName.elementValue).toBe(undefined);
     expect(sovSelfTester.consumerFirstName.statusCode).toBe(StatusCodes.Error);
     expect(sovSelfTester.consumerFirstName.statusMessageKey).toBe(
       StatusMessageKeyTypes.missingConsumerFirstName
     );
-  }
-);
+  },
+});
--- src/tests/page-banner/self-tester/yearOfBirth.test.ts
+++ src/tests/page-banner/self-tester/yearOfBirth.test.ts
@@ -3,11 +3,15 @@ import {
   StatusMessageKeyTypes,
 } from "@src/page-banner/self-tester";
 import { executeOverlayTests } from "../../testUtils";
+import {
+  sovAppDataEverythingIsOkay,
+  sovAppDataNoParameterButIsOkay,
+} from "../sovAppData";
 
-executeOverlayTests(
-  "yearOfBirthSuccess",
-  "everythingIsOkay.html",
-  async (driver, sovSelfTester) => {
+executeOverlayTests({
+  testName: "yearOfBirthSuccess",
+  sovAppData: sovAppDataEverythingIsOkay,
+  testFunction: async (driver, sovSelfTester) => {
     expect(sovSelfTester.consumerYearOfBirth.elementValue).toBe("Smith");
     expect(sovSelfTester.consumerYearOfBirth.statusCode).toBe(
       StatusCodes.Warning
@@ -15,17 +19,19 @@ executeOverlayTests(
     expect(sovSelfTester.consumerYearOfBirth.statusMessageKey).toBe(
       StatusMessageKeyTypes.consumerYearOfBirthSuccess
     );
-  }
-);
+  },
+});
 
-executeOverlayTests(
-  "yearOfBirthMissing",
-  "noParameterButOkay.html",
-  async (driver, sovSelfTester) => {
+executeOverlayTests({
+  testName: "yearOfBirthMissing",
+  sovAppData: sovAppDataNoParameterButIsOkay,
+  testFunction: async (driver, sovSelfTester) => {
     expect(sovSelfTester.consumerYearOfBirth.elementValue).toBe(undefined);
-    expect(sovSelfTester.consumerYearOfBirth.statusCode).toBe(StatusCodes.Error);
+    expect(sovSelfTester.consumerYearOfBirth.statusCode).toBe(
+      StatusCodes.Error
+    );
     expect(sovSelfTester.consumerYearOfBirth.statusMessageKey).toBe(
       StatusMessageKeyTypes.missingConsumerYearOfBirth
     );
-  }
-);
+  },
+});
