572fa5276f514735acbac2ae96212584a444b6ef
sov-drehberg <daniel.rehberg@sovendus.com>
2024-08-21 17:48:46 +0200

    Added all Abo Lists to enabledInBackend Test

--- src/tests/page-banner/self-tester/general/enabledInBackend.test.ts
+++ src/tests/page-banner/self-tester/general/enabledInBackend.test.ts
@@ -66,6 +66,227 @@ executeOverlayTests({
         expectedStatusCode: StatusCodes.Success,
         expectedStatusMessageKey: null,
       },
+
+      {
+        testName: "VNStickyDisabled",
+        sovAppData: {
+          sovIframes1: {
+            ...sovAppIFramesAllValidData,
+            trafficMediumNumber:
+              sovTestTrafficMediumNumbers.VNStickyDisabled.trafficMediumNumber,
+          },
+        },
+        expectedElementValue: false,
+        expectedStatusCode: StatusCodes.Error,
+        expectedStatusMessageKey: StatusMessageKeyTypes.sovendusBannerDisabled,
+      },
+      {
+        testName: "VNStickyEnabled",
+        sovAppData: {
+          sovIframes1: {
+            ...sovAppIFramesAllValidData,
+            trafficMediumNumber:
+              sovTestTrafficMediumNumbers.VNSticky.trafficMediumNumber,
+          },
+        },
+        expectedElementValue: true,
+        expectedStatusCode: StatusCodes.Success,
+        expectedStatusMessageKey: null,
+      },
+
+      {
+        testName: "CBOverlayDisabled",
+        sovAppData: {
+          sovIframes1: {
+            ...sovAppIFramesAllValidData,
+            trafficMediumNumber:
+              sovTestTrafficMediumNumbers.CBOverlayDisabled.trafficMediumNumber,
+          },
+        },
+        expectedElementValue: false,
+        expectedStatusCode: StatusCodes.Error,
+        expectedStatusMessageKey: StatusMessageKeyTypes.sovendusBannerDisabled,
+      },
+      {
+        testName: "CBOverlayEnabled",
+        sovAppData: {
+          sovIframes1: {
+            ...sovAppIFramesAllValidData,
+            trafficMediumNumber:
+              sovTestTrafficMediumNumbers.CBOverlay.trafficMediumNumber,
+          },
+        },
+        expectedElementValue: true,
+        expectedStatusCode: StatusCodes.Success,
+        expectedStatusMessageKey: null,
+      },
+
+      {
+        testName: "CBVNOverlayDisabled",
+        sovAppData: {
+          sovIframes1: {
+            ...sovAppIFramesAllValidData,
+            trafficMediumNumber:
+              sovTestTrafficMediumNumbers.CBVNOverlayDisabled
+                .trafficMediumNumber,
+          },
+        },
+        expectedElementValue: false,
+        expectedStatusCode: StatusCodes.Error,
+        expectedStatusMessageKey: StatusMessageKeyTypes.sovendusBannerDisabled,
+      },
+      {
+        testName: "CBVNOverlayEnabled",
+        sovAppData: {
+          sovIframes1: {
+            ...sovAppIFramesAllValidData,
+            trafficMediumNumber:
+              sovTestTrafficMediumNumbers.CBVNOverlay.trafficMediumNumber,
+          },
+        },
+        expectedElementValue: true,
+        expectedStatusCode: StatusCodes.Success,
+        expectedStatusMessageKey: null,
+      },
+
+      {
+        testName: "CBVNInlineDisabled",
+        sovAppData: {
+          sovIframes1: {
+            ...sovAppIFramesAllValidData,
+            trafficMediumNumber:
+              sovTestTrafficMediumNumbers.CBVNInlineDisabled
+                .trafficMediumNumber,
+          },
+        },
+        expectedElementValue: false,
+        expectedStatusCode: StatusCodes.Error,
+        expectedStatusMessageKey: StatusMessageKeyTypes.sovendusBannerDisabled,
+      },
+      {
+        testName: "CBVNInlineEnabled",
+        sovAppData: {
+          sovIframes1: {
+            ...sovAppIFramesAllValidData,
+            trafficMediumNumber:
+              sovTestTrafficMediumNumbers.CBVNInline.trafficMediumNumber,
+          },
+        },
+        expectedElementValue: true,
+        expectedStatusCode: StatusCodes.Success,
+        expectedStatusMessageKey: null,
+      },
+
+      {
+        testName: "CBInlineDisabled",
+        sovAppData: {
+          sovIframes1: {
+            ...sovAppIFramesAllValidData,
+            trafficMediumNumber:
+              sovTestTrafficMediumNumbers.CBInlineDisabled.trafficMediumNumber,
+          },
+        },
+        expectedElementValue: false,
+        expectedStatusCode: StatusCodes.Error,
+        expectedStatusMessageKey: StatusMessageKeyTypes.sovendusBannerDisabled,
+      },
+      {
+        testName: "CBInlineEnabled",
+        sovAppData: {
+          sovIframes1: {
+            ...sovAppIFramesAllValidData,
+            trafficMediumNumber:
+              sovTestTrafficMediumNumbers.CBInline.trafficMediumNumber,
+          },
+        },
+        expectedElementValue: true,
+        expectedStatusCode: StatusCodes.Success,
+        expectedStatusMessageKey: null,
+      },
+
+      {
+        testName: "CBInlineVNStickyDisabled",
+        sovAppData: {
+          sovIframes1: {
+            ...sovAppIFramesAllValidData,
+            trafficMediumNumber:
+              sovTestTrafficMediumNumbers.CBInlineVNStickyDisabled
+                .trafficMediumNumber,
+          },
+        },
+        expectedElementValue: false,
+        expectedStatusCode: StatusCodes.Error,
+        expectedStatusMessageKey: StatusMessageKeyTypes.sovendusBannerDisabled,
+      },
+      {
+        testName: "CBInlineVNStickyEnabled",
+        sovAppData: {
+          sovIframes1: {
+            ...sovAppIFramesAllValidData,
+            trafficMediumNumber:
+              sovTestTrafficMediumNumbers.CBInlineVNSticky.trafficMediumNumber,
+          },
+        },
+        expectedElementValue: true,
+        expectedStatusCode: StatusCodes.Success,
+        expectedStatusMessageKey: null,
+      },
+
+      {
+        testName: "CBOverlayVNInlineDisabled",
+        sovAppData: {
+          sovIframes1: {
+            ...sovAppIFramesAllValidData,
+            trafficMediumNumber:
+              sovTestTrafficMediumNumbers.CBOverlayVNInlineDisabled
+                .trafficMediumNumber,
+          },
+        },
+        expectedElementValue: false,
+        expectedStatusCode: StatusCodes.Error,
+        expectedStatusMessageKey: StatusMessageKeyTypes.sovendusBannerDisabled,
+      },
+      {
+        testName: "CBOverlayVNInlineEnabled",
+        sovAppData: {
+          sovIframes1: {
+            ...sovAppIFramesAllValidData,
+            trafficMediumNumber:
+              sovTestTrafficMediumNumbers.CBOverlayVNInline.trafficMediumNumber,
+          },
+        },
+        expectedElementValue: true,
+        expectedStatusCode: StatusCodes.Success,
+        expectedStatusMessageKey: null,
+      },
+
+      {
+        testName: "CBOverlayVNStickyDisabled",
+        sovAppData: {
+          sovIframes1: {
+            ...sovAppIFramesAllValidData,
+            trafficMediumNumber:
+              sovTestTrafficMediumNumbers.CBOverlayVNStickyDisabled
+                .trafficMediumNumber,
+          },
+        },
+        expectedElementValue: false,
+        expectedStatusCode: StatusCodes.Error,
+        expectedStatusMessageKey: StatusMessageKeyTypes.sovendusBannerDisabled,
+      },
+      {
+        testName: "CBOverlayVNStickyEnabled",
+        sovAppData: {
+          sovIframes1: {
+            ...sovAppIFramesAllValidData,
+            trafficMediumNumber:
+              sovTestTrafficMediumNumbers.CBOverlayVNSticky.trafficMediumNumber,
+          },
+        },
+        expectedElementValue: true,
+        expectedStatusCode: StatusCodes.Success,
+        expectedStatusMessageKey: null,
+      },
     ],
   }),
 });
