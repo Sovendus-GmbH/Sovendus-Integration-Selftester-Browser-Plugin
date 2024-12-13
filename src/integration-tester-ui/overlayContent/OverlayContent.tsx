2cbef6de79e7d669a41f9143d4bb5c5831700daf
sov-drehberg <daniel.rehberg@sovendus.com>
2024-12-13 16:23:20 +0100

    Changed OverlayContent for Large Overlay

--- src/integration-tester-ui/overlayContent/OverlayContent.tsx
+++ src/integration-tester-ui/overlayContent/OverlayContent.tsx
@@ -1,5 +1,5 @@
 import type { Dispatch, JSX, SetStateAction } from "react";
-import React from "react";
+import React, { useState } from "react";
 
 import type { IntegrationDetectorData } from "../../integration-detector/integrationDetector";
 import type { UiState } from "../../integration-tester-loader/integrationTesterLoader";
@@ -36,111 +36,169 @@ export function OverlayContent({
   }
   return (
     <div style={{ height: "95%", width: "100%", display: "block" }}>
-      {uiState.integrationType === IntegrationType.CB_VN ? (
-        <CBVNContent integrationState={integrationState} />
-      ) : uiState.integrationType === IntegrationType.CHECKOUT_PRODUCTS ? (
-        <CheckoutProductsContent integrationState={integrationState} />
-      ) : (
-        <OptimizeContent integrationState={integrationState} />
-      )}
+      <AccordionContent
+        integrationState={integrationState}
+        uiState={uiState}
+        setUiState={setUiState}
+      />
     </div>
+  );
+}
+
+function AccordionContent({
+  integrationState,
+  uiState,
+  setUiState,
+}: {
+  integrationState: IntegrationDetectorData;
+  uiState: UiState;
+  setUiState: Dispatch<SetStateAction<UiState>>;
+}): JSX.Element {
+  return (
+    <>
+      <CBVNContent
+        integrationState={integrationState}
+        uiState={uiState}
+        setUiState={setUiState}
+      />
+      <CheckoutProductsContent
+        integrationState={integrationState}
+        uiState={uiState}
+        setUiState={setUiState}
+      />
+      <OptimizeContent
+        integrationState={integrationState}
+        uiState={uiState}
+        setUiState={setUiState}
+      />
+    </>
   );
 }
 
 function CBVNContent({
   integrationState,
+  uiState,
+  setUiState,
 }: {
   integrationState: IntegrationDetectorData;
+  uiState: UiState;
+  setUiState: Dispatch<SetStateAction<UiState>>;
 }): JSX.Element {
   return integrationState.selfTester ? (
-    <div style={styles.contentContainer}>
-      <h2 style={styles.sectionTitle}>Sovendus Partner Numbers:</h2>
-      <p style={styles.text}>
-        trafficSourceNumber:{" "}
-        {integrationState.selfTester.trafficSourceNumber.elementValue}
-      </p>
-      <p style={styles.text}>
-        trafficMediumNumber:{" "}
-        {integrationState.selfTester.trafficMediumNumber.elementValue}
-      </p>
+    <div
+      style={styles.contentContainer}
+      onClick={() =>
+        setUiState((prevState) => ({
+          ...prevState,
+          integrationType:
+            prevState.integrationType === IntegrationType.CB_VN
+              ? undefined
+              : IntegrationType.CB_VN,
+        }))
+      }
+    >
+      <h2 style={styles.integrationTitle}>
+        Checkout Benefits & Voucher Network Data:
+      </h2>
+      <div
+        style={{
+          display:
+            uiState.integrationType === IntegrationType.CB_VN
+              ? "block"
+              : "none",
+        }}
+      >
+        <h2 style={styles.sectionTitle}>Sovendus Partner Numbers:</h2>
+        <p style={styles.text}>
+          trafficSourceNumber:{" "}
+          {integrationState.selfTester.trafficSourceNumber.elementValue}
+        </p>
+        <p style={styles.text}>
+          trafficMediumNumber:{" "}
+          {integrationState.selfTester.trafficMediumNumber.elementValue}
+        </p>
 
-      <h2 style={styles.sectionTitle}>Sovendus Container:</h2>
-      <p style={styles.text}>
-        iframeContainerId:{" "}
-        {integrationState.selfTester.iFrameContainerId.elementValue}
-      </p>
+        <h2 style={styles.sectionTitle}>Sovendus Container:</h2>
+        <p style={styles.text}>
+          iframeContainerId:{" "}
+          {integrationState.selfTester.iFrameContainerId.elementValue}
+        </p>
 
-      <h2 style={styles.sectionTitle}>Order Data:</h2>
-      <p style={styles.text}>
-        orderCurrency: {integrationState.selfTester.orderCurrency.elementValue}
-      </p>
-      <p style={styles.text}>
-        orderId: {integrationState.selfTester.orderId.elementValue}
-      </p>
-      <p style={styles.text}>
-        orderValue: {integrationState.selfTester.orderValue.elementValue}
-      </p>
-      <p style={styles.text}>
-        sessionId: {integrationState.selfTester.sessionId.elementValue}
-      </p>
-      <p style={styles.text}>
-        timestamp: {integrationState.selfTester.timestamp.elementValue}
-      </p>
-      <p style={styles.text}>
-        usedCouponCode:{" "}
-        {integrationState.selfTester.usedCouponCode.elementValue}
-      </p>
+        <h2 style={styles.sectionTitle}>Order Data:</h2>
+        <p style={styles.text}>
+          orderCurrency:{" "}
+          {integrationState.selfTester.orderCurrency.elementValue}
+        </p>
+        <p style={styles.text}>
+          orderId: {integrationState.selfTester.orderId.elementValue}
+        </p>
+        <p style={styles.text}>
+          orderValue: {integrationState.selfTester.orderValue.elementValue}
+        </p>
+        <p style={styles.text}>
+          sessionId: {integrationState.selfTester.sessionId.elementValue}
+        </p>
+        <p style={styles.text}>
+          timestamp: {integrationState.selfTester.timestamp.elementValue}
+        </p>
+        <p style={styles.text}>
+          usedCouponCode:{" "}
+          {integrationState.selfTester.usedCouponCode.elementValue}
+        </p>
 
-      <h2 style={styles.sectionTitle}>Customer Data:</h2>
-      <p style={styles.text}>
-        consumerSalutation:{" "}
-        {integrationState.selfTester.consumerSalutation.elementValue}
-      </p>
-      <p style={styles.text}>
-        consumerFirstName:{" "}
-        {integrationState.selfTester.consumerFirstName.elementValue}
-      </p>
-      <p style={styles.text}>
-        consumerLastName:{" "}
-        {integrationState.selfTester.consumerLastName.elementValue}
-      </p>
-      <p style={styles.text}>
-        consumerYearOfBirth:{" "}
-        {integrationState.selfTester.consumerYearOfBirth.elementValue}
-      </p>
-      <p style={styles.text}>
-        consumerDateOfBirth:{" "}
-        {integrationState.selfTester.consumerDateOfBirth.elementValue}
-      </p>
-      <p style={styles.text}>
-        consumerEmail: {integrationState.selfTester.consumerEmail.elementValue}
-      </p>
-      <p style={styles.text}>
-        consumerEmailHash:{" "}
-        {integrationState.selfTester.consumerEmailHash.elementValue}
-      </p>
-      <p style={styles.text}>
-        consumerPhone: {integrationState.selfTester.consumerPhone.elementValue}
-      </p>
-      <p style={styles.text}>
-        consumerStreet:{" "}
-        {integrationState.selfTester.consumerStreet.elementValue}
-      </p>
-      <p style={styles.text}>
-        consumerStreetNumber:{" "}
-        {integrationState.selfTester.consumerStreetNumber.elementValue}
-      </p>
-      <p style={styles.text}>
-        consumerZipcode:{" "}
-        {integrationState.selfTester.consumerZipCode.elementValue}
-      </p>
-      <p style={styles.text}>
-        consumerCity: {integrationState.selfTester.consumerCity.elementValue}
-      </p>
-      <p style={styles.text}>
-        consumerCountry:{" "}
-        {integrationState.selfTester.consumerCountry.elementValue}
-      </p>
+        <h2 style={styles.sectionTitle}>Customer Data:</h2>
+        <p style={styles.text}>
+          consumerSalutation:{" "}
+          {integrationState.selfTester.consumerSalutation.elementValue}
+        </p>
+        <p style={styles.text}>
+          consumerFirstName:{" "}
+          {integrationState.selfTester.consumerFirstName.elementValue}
+        </p>
+        <p style={styles.text}>
+          consumerLastName:{" "}
+          {integrationState.selfTester.consumerLastName.elementValue}
+        </p>
+        <p style={styles.text}>
+          consumerYearOfBirth:{" "}
+          {integrationState.selfTester.consumerYearOfBirth.elementValue}
+        </p>
+        <p style={styles.text}>
+          consumerDateOfBirth:{" "}
+          {integrationState.selfTester.consumerDateOfBirth.elementValue}
+        </p>
+        <p style={styles.text}>
+          consumerEmail:{" "}
+          {integrationState.selfTester.consumerEmail.elementValue}
+        </p>
+        <p style={styles.text}>
+          consumerEmailHash:{" "}
+          {integrationState.selfTester.consumerEmailHash.elementValue}
+        </p>
+        <p style={styles.text}>
+          consumerPhone:{" "}
+          {integrationState.selfTester.consumerPhone.elementValue}
+        </p>
+        <p style={styles.text}>
+          consumerStreet:{" "}
+          {integrationState.selfTester.consumerStreet.elementValue}
+        </p>
+        <p style={styles.text}>
+          consumerStreetNumber:{" "}
+          {integrationState.selfTester.consumerStreetNumber.elementValue}
+        </p>
+        <p style={styles.text}>
+          consumerZipcode:{" "}
+          {integrationState.selfTester.consumerZipCode.elementValue}
+        </p>
+        <p style={styles.text}>
+          consumerCity: {integrationState.selfTester.consumerCity.elementValue}
+        </p>
+        <p style={styles.text}>
+          consumerCountry:{" "}
+          {integrationState.selfTester.consumerCountry.elementValue}
+        </p>
+      </div>
     </div>
   ) : (
     <></>
@@ -149,14 +207,39 @@ function CBVNContent({
 
 function CheckoutProductsContent({
   integrationState,
+  uiState,
+  setUiState,
 }: {
   integrationState: IntegrationDetectorData;
+  uiState: UiState;
+  setUiState: Dispatch<SetStateAction<UiState>>;
 }): JSX.Element {
   return integrationState.selfTester ? (
-    <div style={styles.contentContainer}>
-      <h2 style={styles.sectionTitle}>Checkout Products Data:</h2>
-      <p style={styles.text}>checkoutProductsToken: {""}</p>
-      <p style={styles.text}>checkoutProductsId: {""}</p>
+    <div
+      style={styles.contentContainer}
+      onClick={() =>
+        setUiState((prevState) => ({
+          ...prevState,
+          integrationType:
+            prevState.integrationType === IntegrationType.CHECKOUT_PRODUCTS
+              ? undefined
+              : IntegrationType.CHECKOUT_PRODUCTS,
+        }))
+      }
+    >
+      {" "}
+      <h2 style={styles.integrationTitle}>Checkout Products Data:</h2>
+      <div
+        style={{
+          display:
+            uiState.integrationType === IntegrationType.CHECKOUT_PRODUCTS
+              ? "block"
+              : "none",
+        }}
+      >
+        <p style={styles.text}>checkoutProductsToken: {""}</p>
+        <p style={styles.text}>checkoutProductsId: {""}</p>
+      </div>
     </div>
   ) : (
     <></>
@@ -165,19 +248,44 @@ function CheckoutProductsContent({
 
 function OptimizeContent({
   integrationState,
+  uiState,
+  setUiState,
 }: {
   integrationState: IntegrationDetectorData;
+  uiState: UiState;
+  setUiState: Dispatch<SetStateAction<UiState>>;
 }): JSX.Element {
   return integrationState.selfTester ? (
-    <div style={styles.contentContainer}>
-      <h2 style={styles.sectionTitle}>Optimize Data:</h2>
-      <p style={styles.text}>optimizeId: {""}</p>
-      <p style={styles.text}>checkoutProductsToken: {""}</p>
-      <p style={styles.text}>checkoutProductsId: {""}</p>
-      <p style={styles.text}>legacy_profityId: {""}</p>
-      <p style={styles.text}>
-        couponCode: {integrationState.selfTester.usedCouponCode.elementValue}
-      </p>
+    <div
+      style={styles.contentContainer}
+      onClick={() =>
+        setUiState((prevState) => ({
+          ...prevState,
+          integrationType:
+            prevState.integrationType === IntegrationType.OPTIMIZE
+              ? undefined
+              : IntegrationType.OPTIMIZE,
+        }))
+      }
+    >
+      {" "}
+      <h2 style={styles.integrationTitle}>Optimize Data:</h2>
+      <div
+        style={{
+          display:
+            uiState.integrationType === IntegrationType.OPTIMIZE
+              ? "block"
+              : "none",
+        }}
+      >
+        <p style={styles.text}>optimizeId: {""}</p>
+        <p style={styles.text}>checkoutProductsToken: {""}</p>
+        <p style={styles.text}>checkoutProductsId: {""}</p>
+        <p style={styles.text}>legacy_profityId: {""}</p>
+        <p style={styles.text}>
+          couponCode: {integrationState.selfTester.usedCouponCode.elementValue}
+        </p>
+      </div>
     </div>
   ) : (
     <></>
@@ -186,23 +294,31 @@ function OptimizeContent({
 
 const styles: {
   contentContainer: React.CSSProperties;
+  integrationTitle: React.CSSProperties;
   sectionTitle: React.CSSProperties;
   text: React.CSSProperties;
 } = {
   contentContainer: {
-    padding: "20px",
-    paddingTop: "0px",
+    padding: "2px 5px 0px 5px",
     backgroundColor: "#fff",
     borderRadius: "8px",
     boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
     margin: "10px",
     overflowY: "auto",
     maxHeight: "calc(100% - 50px)",
+    cursor: "pointer",
+  },
+  integrationTitle: {
+    color: "black",
+    fontSize: "22px",
+    margin: "5px",
+    borderBottom: "2px solid #ccc",
+    paddingBottom: "5px",
   },
   sectionTitle: {
-    color: "#333",
-    fontSize: "18px",
-    marginBottom: "10px",
+    color: "black",
+    fontSize: "15px",
+    marginBottom: "5px",
     borderBottom: "1px solid #ccc",
     paddingBottom: "5px",
   },
@@ -210,6 +326,7 @@ const styles: {
     color: "#000",
     fontSize: "14px",
     lineHeight: "1.6",
+    marginLeft: "5px",
     marginBottom: "8px",
   },
 };
