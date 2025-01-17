a802f0e4dc0efc1058eacdc0d001e77226e4ca28
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-01-17 20:19:59 +0100

    disable api transmission for browser extension

--- src/integration-tester-loader/browserExtensionLoader.ts
+++ src/integration-tester-loader/browserExtensionLoader.ts
@@ -1,4 +1,9 @@
+import type { SovSelfTesterWindow } from "../integration-tester/integrationTester";
 import { startIntegrationTester } from "./integrationTesterLoader";
 
 // TODO add blacklist storage
+
+window.transmitTestResult = false;
 void startIntegrationTester([]);
+
+declare let window: SovSelfTesterWindow;
